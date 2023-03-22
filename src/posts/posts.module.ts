a3b9cbe918e2f7ae74d1e2d5d9ee672c92aec194
ecoarchie <eco.artur@gmail.com>
2023-03-22 19:36:57 +0300

    fix: add accessTokenValidation to posts controller

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,5 +1,14 @@
-import { forwardRef, Module } from '@nestjs/common';
+import {
+  forwardRef,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { BlogsModule } from 'src/blogs/blogs.module';
+import { AccessTokenValidationMiddleware } from 'src/middlewares/accessTokenCheck.middleware';
+import { UsersModule } from 'src/users/users.module';
+import { JwtService } from 'src/utils/jwt.service';
 import { PostsController } from './posts.controller';
 import { PostsQueryRepository } from './posts.query-repository';
 import { PostsRepository } from './posts.repository';
@@ -7,8 +16,20 @@ import { PostsService } from './posts.service';
 
 @Module({
   controllers: [PostsController],
-  providers: [PostsService, PostsRepository, PostsQueryRepository],
-  imports: [forwardRef(() => BlogsModule)],
+  providers: [PostsService, PostsRepository, PostsQueryRepository, JwtService],
+  imports: [forwardRef(() => BlogsModule), forwardRef(() => UsersModule)],
   exports: [PostsQueryRepository, PostsRepository, PostsService],
 })
-export class PostsModule {}
+export class PostsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AccessTokenValidationMiddleware)
+      .exclude(
+        { path: 'posts', method: RequestMethod.POST },
+        { path: 'posts/:id', method: RequestMethod.DELETE },
+        { path: 'posts/:postId/comments', method: RequestMethod.POST },
+        { path: 'posts/:postId/like-status', method: RequestMethod.PUT },
+      )
+      .forRoutes(PostsController);
+  }
+}
